936e982e29073207c4f82340167c9ba312c0f7a9
ouxuwen <mo22398291@163.com>
2018-03-06 16:32:39 +0800

    Update BaiduFace.js

--- www/BaiduFace.js
+++ www/BaiduFace.js
@@ -5,10 +5,6 @@ BaiduFace.prototype.open = function (arg0, success, error) {
     cordova.exec(success, error, 'BaiduFace', 'open', [arg0]);
 
 }
-BaiduFace.prototype.test = function (arg0, success, error) {
-    cordova.exec(success, error, 'BaiduFace', 'test', [arg0]);
-}
-BaiduFace.prototype.puejie = function (arg0, success, error) {
-    alert("pu");
-}
+
+
 module.exports = new BaiduFace();
